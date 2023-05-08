0f4b035d02a2d66097f53969ecbdae35c318d446
Fedekh <federicocet@gmail.com>
2023-05-08 13:25:07 +0200

    create project scaffolding

---
+++ index.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="shortcut icon" href="./img/logo.jpg" />
+    <!-- AXIOS -->
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/axios/1.4.0/axios.min.js" integrity="sha512-uMtXmF28A2Ab/JJO2t/vYhlaa/3ahUOgj1Zf27M5rOo8/+fcTUVH0/E0ll68njmjrLqOBjXM3V9NiPFL5ywWPQ==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+
+    <!-- VUE JS -->
+    <script src="https://unpkg.com/vue@3/dist/vue.global.js"></script>
+    <!-- BOOTSTRAP -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
+
+    <!-- CSS -->
+    <link rel="stylesheet" href="./css/style.css">
+    <title>Spotify</title>
+</head>
+
+<body>
+
+    <div id="app">
+        <header>
+            <section class="container d-flex align-items-center">
+                <div>
+                    <img src="./img/logo.png" alt="">
+                </div>
+            </section>
+        </header>
+        <main class="ms_container d-flex my-5 flex-wrap">
+                <div class="prova"></div>
+                <div class="prova"></div>
+                <div class="prova"></div>
+                <div class="prova"></div>
+                <div class="prova"></div>
+                <div class="prova"></div>
+        </main>
+        <script src="./js/script.js"></script>
+    </div>
+</body>
+
+</html>
