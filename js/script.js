284debd17dd434df41777f88192539f6395fccd6
Fedekh <federicocet@gmail.com>
2023-05-08 18:54:16 +0200

    refix bonus with apllying a filter back-end side while api call

--- js/script.js
+++ js/script.js
@@ -4,23 +4,30 @@ createApp({
     data() {
         return {
             discs: [],
-            isClicked: false,
-            cardSelected: '',
+            isClicked: false,       //variabile per mostrare i dettagli del disco
+            cardSelected: {},   //oggetto vuoto che conterrà i dati del disco selezionato
 
         }
     },
     mounted() {
         axios
-            .get('http://localhost:8888/Boolean%20Server%20Fake/php-dischi-json/server.php')
+            .get('http://localhost:8888/Boolean%20Fake%20Server/php-dischi-json/server.php')
             .then(response => {
                 this.discs = response.data.results;
-                console.log(this.discs);
             })
     },
     methods: {
-        showDetails(index) {
+        showDetails(index) {        //funzione per mostrare i dettagli del disco
             this.isClicked = true;
-            this.cardSelected = this.discs[index];
+             axios
+            .get('http://localhost:8888/Boolean%20Fake%20Server/php-dischi-json/server.php',
+            {params:{
+                choise:index
+            }})
+            .then(response => {       
+                this.cardSelected= response.data.results
+            })
+            
         },
        
     }
--- server.php
+++ server.php
@@ -1,16 +1,23 @@
 <?php
 
 $string_value = file_get_contents('./dischi.json');
-// var_dump($string_value);
 $dischi_array = json_decode($string_value, true);
-// var_dump($dischi_array);
 
+// Mi creo un filtro back-end
 
-// Preparazione della risposta
-$results = [
-    "results" => $dischi_array,
-    "success" => true
-];
+if (isset($_GET['choise'])) {
+    $results = [
+        "results" => $dischi_array[$_GET['choise']],
+        "success" => true
+    ];
+}else{
+    $results = [
+        "results" => $dischi_array,
+        "success" => true
+    ];
+
+}
+
 
 header('Content-Type: application/json');
 echo json_encode($results);
